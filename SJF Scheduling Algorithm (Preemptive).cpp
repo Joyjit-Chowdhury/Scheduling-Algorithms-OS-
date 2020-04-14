ab62c22b4fed14780603f4e0aacdd6ac281abe40
Joyjit Chowdhury <53374920+Joyjit-Chowdhury@users.noreply.github.com>
2020-04-14 17:27:08 +0600

    Update SJF Scheduling Algorithm (Preemptive).cpp

--- SJF Scheduling Algorithm (Preemptive).cpp
+++ SJF Scheduling Algorithm (Preemptive).cpp
@@ -1,66 +1,60 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     ios::sync_with_stdio(0);
 
-    int n, complete=0,minm=0,c=0;
+    int i, smallestIndex, processCompleted = 0, time, n;
+    float awt = 0, atat = 0;
     cout << "Enter the Number of Processes: ";
     cin >> n;
-
-    int p[n], bt[n], a_t[n], ct[n], tat[n], wt[n], abt[n+1];
-    float avgwt = 0, avgtat = 0;
-
-    for(int i = 0; i < n; i++)
+    int p[n], a_t[n], bt[n + 1], temp[n], wt[n], tat[n], completion[n];
+    for(i = 0; i < n; i++)
     {
         p[i] = i + 1;
-        cout << "Enter Arrival Time and Burst Time for Process " << i+1 << ": ";
-        cin >> a_t[i] >> bt[i];
+        cout << "Enter Burst Time and Arrival Time for Process " << i+1 << ": ";
+        cin >> bt[i] >> a_t[i];
+        temp[i] = bt[i];
     }
-    abt[n] = 9999;
-    for(int i=0;i<n;i++)
+    bt[n] = 9999;
+
+    for(time = 0; processCompleted < n; time++)
     {
-        complete += bt[i];
-        abt[i] = bt[i];
-    }
-    while(c != n)
-    {
-        for(int t = 0; t < complete; t++)
+
+        smallestIndex = n;
+        for(i = 0; i < n; i++)
         {
-            minm = n;
-            for(int i = 0; i < n; i++)
+            if(a_t[i] <= time && bt[i] < bt[smallestIndex] && bt[i] > 0)
             {
-                if(a_t[i] <= t && abt[i] < abt[minm] && abt[i] != 0)
-                    minm = i;
+                smallestIndex = i;
             }
-            abt[minm]--;
+        }
+        bt[smallestIndex] = bt[smallestIndex] -1;
 
-            if(abt[minm] == 0)
-            {
-                c++;
-                ct[minm] = t+1;
+        if(bt[smallestIndex] == 0)
+        {
+            processCompleted++;
+            completion[smallestIndex] = time + 1;
 
-            }
+            tat[smallestIndex] = completion[smallestIndex] - a_t[smallestIndex];
+            wt[smallestIndex] = tat[smallestIndex] - temp[smallestIndex];
+
+            awt = awt + wt[smallestIndex];
+            atat = atat + tat[smallestIndex];
         }
     }
 
-    for(int i = 0; i < n; i++)
-    {
-        tat[i] = ct[i] - a_t[i];
-        wt[i] = tat[i] - bt[i];
-        avgwt += wt[i];
-        avgtat += tat[i];
-    }
     cout << "\nProcess" << "\t\tArrival Time" << "\tBurst Time" << "\tCompletion Time" << "\t\tTurn Around Time" <<
          "\tWaiting Time" << endl;
-    for(int i = 0; i < n; i++)
+
+    for(i = 0; i < n; i++)
     {
-        cout << p[i] << "\t\t" << a_t[i] << "\t\t" << bt[i] << "\t\t" << ct[i] << "\t\t\t" << tat[i] <<
-             "\t\t\t" << wt[i] << endl;
+        cout << p[i] << "\t\t" << a_t[i] << "\t\t" << temp[i] << "\t\t" << completion[i] << "\t\t\t" << tat[i] << "\t\t\t" << wt[i] << endl;
     }
-    cout << fixed << setprecision(2) << "\nAverage Waiting Time: " << avgwt/n << endl;
-    cout << fixed << setprecision(2) << "Average Turnaround Time: " << avgtat/n << endl;
+
+    cout << fixed << setprecision(2) << "\nAverage Waiting Time: " << awt/n << endl;
+    cout << fixed << setprecision(2) << "Average Turnaround Time: " << atat/n << endl;
 
     return 0;
 }
